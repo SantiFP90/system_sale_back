887da294e80505d4d50d8c7b65180cfc2e32e1ec
Santiago Fernandez Pensa <sfernandez@SOFTWARE-FACTORY.ENCODESA.INTERNAL>
2025-02-13 14:37:11 -0300

    sql

--- SistemaVentas.DAL/obj/Debug/net7.0/SistemaVentas.DAL.AssemblyInfo.cs
+++ SistemaVentas.DAL/obj/Debug/net7.0/SistemaVentas.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVentas.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e836628ba05b4b3fa8a281c2ea0576fa906ca82c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVentas.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVentas.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
